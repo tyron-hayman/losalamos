455f6e21a1d1dd122e01706a65fa6df2eb85bd1c
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-03-06 13:33:36 -0800

    Nav component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-[10vw] font-black text-white">
+      <h1 className="text-[13vw] font-black text-white">
         AI + Inspiration
       </h1>
     </div>
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
 export default function MainNav({ title }: Readonly<{ title: string }>) {
+  const navItems: Array<{ id: string; title: string; link: string }> = [
+    { id: "nav01", title: "Home", link: "/" },
+    { id: "nav02", title: "Explore", link: "/explore" },
+  ];
+
   return (
     <div className="fixed top-0 inset-x-0 p-5 flex items-center justify-between">
       <div>
@@ -11,8 +16,19 @@ export default function MainNav({ title }: Readonly<{ title: string }>) {
           {title}
         </Link>
       </div>
-      <div>
-        <Link href="/about">Explore</Link>
+      <div className="flex gap-5 items-center justify-end">
+        {navItems.map((item, index) => (
+          <Link
+            key={item.id}
+            className="text-white text-lg font-bold"
+            href={item.link}
+          >
+            <span className="text-zinc-700 text-sm pr-2 font-black">
+              0{index + 1}
+            </span>
+            {item.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
